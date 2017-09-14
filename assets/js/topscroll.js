3f223d65f411090bae2e716afed48aab3d5b67f4
fenix_63@mail.ru <youran_88>
2017-09-14 22:43:06 +0400

    Добавил слайдер, добавил кнопку "Наверх"

---
+++ assets/js/topscroll.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function(){
+// hide #back-top first
+    $("#back-top").hide();
+     
+    // fade in #back-top
+    
+        //alert('test');
+        //alert($(document).scrollTop());
+
+        $(window).scroll(function () {
+            console.log($(document).scrollTop());
+            if ($(this).scrollTop() > 100) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+                }
+            });
+
+
+        // scroll body to 0px on click
+        $('#back-top a').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+
+            return false;
+        });
+    
+
+
+});
